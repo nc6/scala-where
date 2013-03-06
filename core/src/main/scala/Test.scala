a4ee265971ae7964d203d9c18a7b2d601ffc9bce
Nicholas Clarke <nc6@sanger.ac.uk>
2013-03-06 16:43:34 +0000

    First drop of a scala 'where' construct.

---
+++ core/src/main/scala/Test.scala
@@ -0,0 +1,13 @@
+import uk.org.topos.scalaWhere.Macros.where
+
+object Test extends App {
+
+  val result = where ( f1(1) * f2(2), {
+  	def f1(x : Int) = x + 1
+  	def f2(x : Int) = x + 2
+  })
+
+  println(result)
+
+
+}
---
+++ macros/src/main/scala/Macros.scala
@@ -0,0 +1,17 @@
+package uk.org.topos.scalaWhere
+
+import language.experimental.macros
+import scala.reflect.macros.Context
+
+object Macros {
+
+	def where(exp : _, block : Unit) = macro where_impl
+
+	def where_impl(c : Context)(exp : c.Tree, block : c.Expr[Unit]) = {
+		import c.universe._
+
+		val Expr(Block((inner, _))) = block
+		val newinner = inner :+ exp
+		Block(newinner : _*)
+	}
+}
