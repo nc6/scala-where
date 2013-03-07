2680b2624d441bddbb7f0aa9880ef78ee4328858
Nicholas Clarke <nc6@sanger.ac.uk>
2013-03-07 09:15:02 +0000

    Use two argument lists to get control flow structure.

--- core/src/main/scala/Test.scala
+++ core/src/main/scala/Test.scala
@@ -2,12 +2,10 @@ import uk.org.topos.scalaWhere.Macros.where
 
 object Test extends App {
 
-  val result = where ( f1(1) * f2(2), {
+  val result = where (f1(1) * f2(2)) {
   	def f1(x : Int) = x + 1
   	def f2(x : Int) = x + 2
-  })
+  }
 
   println(result)
-
-
 }
--- macros/src/main/scala/Macros.scala
+++ macros/src/main/scala/Macros.scala
@@ -5,9 +5,9 @@ import scala.reflect.macros.Context
 
 object Macros {
 
-	def where(exp : _, block : Unit) = macro where_impl
+	def where(exp : _)(block : Unit) = macro where_impl
 
-	def where_impl(c : Context)(exp : c.Tree, block : c.Expr[Unit]) = {
+	def where_impl(c : Context)(exp : c.Tree)(block : c.Expr[Unit]) = {
 		import c.universe._
 
 		val Expr(Block((inner, _))) = block
